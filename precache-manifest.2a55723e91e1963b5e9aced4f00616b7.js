19fe982a68586baa9984011007a3581f25580233
aKass <aKass@users.noreply.github.com>
2020-09-05 19:24:09 +0200

    Updates

---
+++ precache-manifest.2a55723e91e1963b5e9aced4f00616b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27d17c39559e2d8f3104e3a7e66ebf5c",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "0b47799566bd46e3162b",
+    "url": "/react-monsters/static/css/main.9f761e48.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/react-monsters/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b47799566bd46e3162b",
+    "url": "/react-monsters/static/js/main.df93237d.chunk.js"
+  },
+  {
+    "revision": "e23079873de2af122ba1",
+    "url": "/react-monsters/static/js/runtime-main.d4df2b23.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-monsters/precache-manifest.baff8616dd67fd61c739c4e54118dc79.js"
+  "/react-monsters/precache-manifest.2a55723e91e1963b5e9aced4f00616b7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-monsters/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-monsters/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
